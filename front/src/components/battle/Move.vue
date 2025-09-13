22e9bb02b46c56ce9ff563f327c71b6c2e4ca004
MelvinCourant <melvin.courant1@gmail.com>
2025-09-13 12:39:38 +0200

    :bento: update pokemon ds font cause i un uppercase was stacked to right and font size divided by 2

--- front/src/components/battle/Move.vue
+++ front/src/components/battle/Move.vue
@@ -19,7 +19,7 @@ function generateImageSrc(move) {
     @click="$emit('moveSelected', move.id)"
     :disabled="move.pp === 0"
   >
-    <h3 class="move__title">{{ move.name }}</h3>
+    <h3 class="move__name">{{ move.name }}</h3>
     <p class="move__pp">{{ move.pp }}/{{ move.maxPp }}</p>
     <img
       class="move__background"
